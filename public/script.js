3f6810a45b84198c9e274a4242f86e97de4ed892
Ryank2004 <ryankorver04@gmail.com>
2024-03-26 21:34:25 +0100

    commit

--- public/script.js
+++ public/script.js
@@ -8,27 +8,48 @@ hamburger.addEventListener("click", () => {
 });
 
 //Javascript voor invliegende animatie
-let reveals = document.querySelectorAll(".reveal");
+// let reveals = document.querySelectorAll(".reveal");
 
-window.addEventListener("scroll", reveal);
-window.addEventListener("scroll", checkHeight);
+// window.addEventListener("scroll", reveal);
+// window.addEventListener("scroll", checkHeight);
 
-function reveal() {
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
+// function reveal() {
+//     for (var i = 0; i < reveals.length; i++) {
+//       var windowHeight = window.innerHeight;
+//       var elementTop = reveals[i].getBoundingClientRect().top;
+//       var elementVisible = 150;
   
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
+//       if (elementTop < windowHeight - elementVisible) {
+//         reveals[i].classList.add("active");
+//       } else {
+//         reveals[i].classList.remove("active");
+//       }
+//     }
+//   }
+  
+  
+//   window.addEventListener("resize", checkHeight);
+  
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Krijg de huidige path van de URL.
+  const currentPath = window.location.pathname;
+
+  // Loop door alle navigatielinks.
+  document.querySelectorAll('.lijst-item').forEach(link => {
+      // Krijg het pad van de href attribuut voor de link.
+      const linkPath = link.getAttribute('href');
+
+      // Verwijder de 'active-state' klasse van alle links eerst
+      link.classList.remove('active-state');
+
+      // Controleer of de link overeenkomt met de huidige pagina.
+      // Verwijder eventuele leidende en volgende slashes voor een juiste vergelijking.
+      if (currentPath.replace(/^\/|\/$/g, '') === linkPath.replace(/^\/|\/$/g, '')) {
+          // Voeg de 'active-state' klasse toe aan de overeenkomende link.
+          link.classList.add('active-state');
       }
-    }
-  }
-  
-  
-  window.addEventListener("resize", checkHeight);
-  
+  });
+});
 
-  
+
